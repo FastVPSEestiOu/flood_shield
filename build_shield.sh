c5102edaa007fd92d0c4774ba954c94c4646a1a2
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-18 19:15:35 +0300

    Initial commit; Project moved from FastNetMon repository: https://github.com/FastVPSEestiOu/fastnetmon

---
+++ build_shield.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+g++ ipset_management.cpp -lipset -c -o ipset_management.o -I/opt/ipset/include -L/opt/ipset/lib
+gcc picohttpparser.c -c -o picohttpparser.o
+g++ shield.cpp picohttpparser.o ipset_management.o -opicohttpparser -oshield -I/opt/pf_ring/include -L/opt/pf_ring/lib -lpfring -lnuma -lpcap -lipset -L/opt/ipset/lib
+
+rm -f picohttpparser.o ipset_management.o ipset.o fastnetmon_packet_parser.o
